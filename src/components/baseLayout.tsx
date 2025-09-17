e24a2868ac23a525a4373b04d677066f5e8b1700
yizuohua <yizuohua@gmail.com>
2025-09-17 18:34:28 +0800

    feat:  根据routes自动生成menus

--- src/components/baseLayout.tsx
+++ src/components/baseLayout.tsx
@@ -1,35 +1,50 @@
-import { NavLink, Outlet } from "react-router";
-
-const routes = [
-	{
-		path: "/useState",
-		label: "useState",
-	},
-	{
-		path: "/useReducer",
-		label: "useReducer",
-	},
-	{
-		path: "/useSyncExternalStore",
-		label: "useSyncExternalStore",
-	},
-];
+import { useState, useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { Menu } from "antd";
+import { menus, type MenuItem } from "@/router/routes";
 
 export default function BaseLayout() {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [current, setCurrent] = useState("");
+
+	//  根据地址栏查找当前激活菜单
+	function findCurrentMenu(menus: MenuItem[], path: string) {
+		const menu = menus.find((menu) => menu.key === path) ?? menus[0];
+		const recursiveFind = (menu: MenuItem, path: string) => {
+			if (menu.children && menu.children.length) {
+				const child = menu.children[0];
+				return recursiveFind(child, path);
+			}
+			return menu.key;
+		};
+		return recursiveFind(menu, path);
+	}
+
+	function handleClick({ item, key, keyPath }: { item: any; key: string; keyPath: string[] }) {
+		console.log(item, key, keyPath);
+		setCurrent(key);
+		navigate(key);
+	}
+
+	useEffect(() => {
+		const path = findCurrentMenu(menus, location.pathname);
+		setCurrent(path);
+		navigate(path);
+	}, []);
+
 	return (
 		<div className="min-h-screen bg-gray-100">
 			<div className="flex gap-4 p-4">
-				{routes.map((route) => (
-					<NavLink
-						key={route.path}
-						to={route.path}
-						className={({ isActive }) => (isActive ? "text-primary" : "text-gray-500")}
-					>
-						{route.label}
-					</NavLink>
-				))}
+				<Menu
+					className="w-full"
+					onClick={handleClick}
+					items={menus}
+					selectedKeys={[current]}
+					mode="horizontal"
+				/>
 			</div>
-			<div className="mt-4">
+			<div>
 				<Outlet />
 			</div>
 		</div>
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,29 +1,46 @@
 import { lazy } from "react";
+import type { ReactNode } from "react";
 import type { RouteObject } from "react-router";
-import { LazyImport } from "@/components";
+import { LazyImport, StaticLayout } from "@/components";
 
-export type RouteConfig = RouteObject & {
-	meta?: {
-		[key: string]: any;
-	};
-};
-
-export const routes: RouteConfig[] = [
+export const routes: RouteObject[] = [
 	{
 		path: "/",
 		element: <LazyImport lazy={lazy(() => import("@/components/baseLayout"))} />,
 		children: [
 			{
-				index: true,
-				path: "useState",
-				Component: lazy(() => import("@/pages/useState")),
+				path: "state",
+				handle: {
+					label: "数据状态",
+					title: "数据状态",
+				},
+				element: <StaticLayout />,
+				children: [
+					{
+						index: true,
+						path: "useState",
+						handle: {
+							label: "useState",
+							title: "useState",
+						},
+						element: <LazyImport lazy={lazy(() => import("@/pages/useState"))} />,
+					},
+				],
 			},
 			{
 				path: "useReducer",
+				handle: {
+					label: "状态管理",
+					title: "状态管理",
+				},
 				Component: lazy(() => import("@/pages/useReducer")),
 			},
 			{
 				path: "useSyncExternalStore",
+				handle: {
+					label: "订阅状态",
+					title: "订阅状态",
+				},
 				Component: lazy(() => import("@/pages/useSyncExternalStore")),
 			},
 		],
@@ -33,3 +50,56 @@ export const routes: RouteConfig[] = [
 		Component: lazy(() => import("@/pages/NotFound")),
 	},
 ];
+
+export type MenuItem = {
+	key: string;
+	label: string | ReactNode;
+	title: string;
+	children?: MenuItem[];
+};
+
+function routesToAntdMenu(routes: RouteObject[]): MenuItem[] {
+	const result: MenuItem[] = [];
+
+	function handle(route: RouteObject, parentPath = "", parentItem: MenuItem | null = null) {
+		const fullPath = route.path
+			? route.path.startsWith("/")
+				? route.path
+				: `${parentPath}/${route.path}`.replace(/\/+/g, "/")
+			: parentPath;
+
+		const menuItem: MenuItem = {
+			key: fullPath,
+			label: route.handle?.label ?? "",
+			title: route.handle?.title ?? "",
+		};
+
+		if (route.handle && route.handle.label) {
+			if (parentItem) {
+				if (!parentItem.children) {
+					parentItem.children = [];
+				}
+				parentItem.children.push(menuItem);
+			} else {
+				result.push(menuItem);
+			}
+		}
+
+		const children = (route.children || []).filter((item) => item.handle && item.handle.label);
+		if (children.length) {
+			for (let i = 0; i < children.length; i++) {
+				handle(children[i], fullPath, menuItem.label ? menuItem : null);
+			}
+		}
+	}
+
+	for (let i = 0; i < routes.length; i++) {
+		handle(routes[i]);
+	}
+
+	return result;
+}
+
+export const menus = routesToAntdMenu(routes);
+
+console.log("页面路由", menus);
